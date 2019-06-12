088da35885b2b43f164a2cb0f92d5fbbb683b668
Eduard Climov <klimovedik88@gmail.com>
2019-06-12 22:52:11 +0300

    Initial commit

---
+++ src/components/layouts/CommonLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-layout row wrap class="mt-4">
+      <v-flex xs8 class="pa-5">
+        <router-view />
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CommonLayout'
+}
+</script>
+
+<style scoped>
+</style>
